5b0bd5754b62f93e479aa25ccf49da7f8369ff5a
Jacob Greenwald <greenwald.j8@gmail.com>
2022-01-25 22:28:38 -0500

    Working on types

--- src/interface/concerts.interface.ts
+++ src/interface/concerts.interface.ts
@@ -1,14 +1,39 @@
-interface ConcertListParams {
-  artistName: string;
-  artistYear?: string;
-}
-
-interface SingleConcert {
-  description: string;
+export interface SingleConcert {
+  description?: string;
   identifier: string;
   mediatype: string;
   title: string;
   year: number;
+  date: string;
 }
 
-type PaginatedConcertList = SingleConcert[][];
+export type PaginatedConcertList = SingleConcert[][];
+
+enum SortOrder {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
+// Passed from front-end
+export interface ConcertSearchOptions {
+  searchTerm: string; // 'Grateful+Dead+AND+year%3A1977'
+  max: number;
+  sortOrder: SortOrder;
+}
+
+// Passed into archiveSearch library
+export interface ArchiveSearchOptions {
+  searchBy: string;
+  fields: string[];
+  max: number;
+  sortBy: SortByOptions;
+}
+
+type SortByOptions = Record<string, SortOrder>;
+
+// Response from archive's api
+export interface SearchResponse {
+  numFound: number;
+  start: number;
+  docs: SingleConcert[];
+}
