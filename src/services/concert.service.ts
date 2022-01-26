c39e8980704ced3b8b1e2ddf4935011731797d10
Jacob Greenwald <greenwald.j8@gmail.com>
2022-01-25 22:29:11 -0500

    Making request and formatting response

--- src/services/concert.service.ts
+++ src/services/concert.service.ts
@@ -1,9 +1,33 @@
+import { archiveSearch } from 'archive-search';
 import { Injectable } from '@nestjs/common';
+import { baseOptions, formatResponse } from './concert.util';
+import {
+  ConcertSearchOptions,
+  ArchiveSearchOptions,
+  SearchResponse,
+  PaginatedConcertList,
+} from '../interface/concerts.interface';
+
 import * as concertListMock from '../../test/mocks/concertListMock.json';
+import * as searchResponse from '../../test/mocks/searchResponse.json';
 
 @Injectable()
 export class ConcertService {
-  getConcertList(artistName: string, artistYear: string): any {
-    return concertListMock;
+  async getConcertList({
+    searchTerm,
+    max,
+    sortOrder,
+  }: ConcertSearchOptions): Promise<PaginatedConcertList> {
+    const searchOptions: ArchiveSearchOptions = {
+      ...baseOptions,
+      max,
+      sortBy: { date: sortOrder },
+    };
+    // const searchConcerts: SearchResponse = await archiveSearch.search(
+    //   searchTerm,
+    //   searchOptions,
+    // );
+    const searchConcerts: SearchResponse = searchResponse;
+    return formatResponse(searchConcerts);
   }
 }
