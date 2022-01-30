f495a368b04af5db1855ab3e8e6f5ea2e75e3cbf
Jacob Greenwald <greenwald.j8@gmail.com>
2022-01-29 22:29:04 -0500

    Remove unused service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConcertModule } from './services/concert.module';
 
 @Module({
   imports: [ConcertModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
