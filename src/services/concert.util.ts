468f6f399eca19b515c1c6ff5abd48709e690c97
Jacob Greenwald <greenwald.j8@gmail.com>
2022-02-12 16:52:35 -0500

    Logic to dedupe concerts

--- src/services/concert.util.ts
+++ src/services/concert.util.ts
@@ -1,21 +1,71 @@
+import { BadRequestException } from '@nestjs/common';
 import chunk from 'lodash.chunk';
 import {
   BaseSearchOptions,
   ETREE,
+  MediaFormat,
   PaginatedConcertList,
   SearchResponse,
+  SingleConcert,
 } from '../interface/concerts.interface';
+
+const { MP3, OGG } = MediaFormat;
 
 export const baseOptions: BaseSearchOptions = {
   searchBy: 'creator',
-  fields: ['description', 'identifier', 'mediatype', 'title', 'year', 'date'],
+  fields: [
+    'avg_rating',
+    'reviews',
+    'coverage',
+    'creator',
+    'date',
+    'description',
+    'downloads',
+    'format',
+    'licenseurl',
+    'mediatype',
+    'name',
+    'num_reviews',
+    'source',
+    'title',
+    'year',
+  ],
+};
+
+const includesFormat = (array: string[]) => {
+  return [OGG, MP3].some((format) => array.indexOf(format) !== -1);
+};
+
+const isProperFormat = (mediatype: string, format: string[]) => {
+  return mediatype === ETREE && includesFormat(format);
 };
 
 export function paginateResponse(
   searchResponse: SearchResponse,
+  filterDuplicates: boolean,
 ): PaginatedConcertList {
-  const filterMediaType = searchResponse.docs.filter(
-    ({ mediatype }) => mediatype === ETREE,
+  const filterAndDedupe = searchResponse.docs.reduce<SingleConcert[]>(
+    (acc, curr) => {
+      if (filterDuplicates) {
+        if (
+          isProperFormat(curr.mediatype, curr.format) &&
+          !acc.some(({ title }) => title === curr.title)
+        ) {
+          return acc.concat(curr);
+        }
+        return acc;
+      }
+      if (isProperFormat(curr.mediatype, curr.format)) {
+        return acc.concat(curr);
+      }
+      return acc;
+    },
+    [],
   );
-  return chunk(filterMediaType, 30);
+
+  if (!filterAndDedupe.length) {
+    throw new BadRequestException('No results');
+  }
+
+  return chunk(filterAndDedupe, 30);
 }
