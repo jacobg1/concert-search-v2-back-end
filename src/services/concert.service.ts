f6f96ed80af47b9c1efac8e6eb62eb19b3cb13e5
Jacob Greenwald <greenwald.j8@gmail.com>
2022-02-12 16:52:19 -0500

    Passing in filterDuplicates and update files key to trackList

--- src/services/concert.service.ts
+++ src/services/concert.service.ts
@@ -8,6 +8,7 @@ import {
   ConcertData,
   MediaFormat,
   ArchiveSearchOptions,
+  ConcertResponse,
 } from '../interface/concerts.interface';
 
 // import * as concertListMock from '../../test/mocks/concertListMock.json';
@@ -18,34 +19,32 @@ export class ConcertService {
   async getConcertList({
     searchTerm,
     max,
-    sortOrder,
+    sortBy,
     filterDuplicates,
   }: ConcertSearchOptions): Promise<PaginatedConcertList> {
     const searchOptions: ArchiveSearchOptions = {
       ...baseOptions,
       max,
-      sortBy: { date: sortOrder },
-      filterDuplicates,
+      sortBy,
     };
     const searchConcerts: SearchResponse = await archiveSearch.search(
       searchTerm,
       searchOptions,
     );
     // const searchConcerts: any = searchResponse;
-    return paginateResponse(filterDuplicates ? searchConcerts : searchConcerts); // TODO: implement dedupe
+    return paginateResponse(searchConcerts, filterDuplicates);
   }
-
   async getSingleConcert(
     id: string,
     format: MediaFormat,
   ): Promise<ConcertData> {
     const selectedFormat = MediaFormat[format.toUpperCase()];
-    const concert: ConcertData = await archiveSearch.metaSearch(id);
+    const concert: ConcertResponse = await archiveSearch.metaSearch(id);
 
     const { metadata, files } = concert;
     return {
       metadata,
-      files: files.filter((file) => file.format === selectedFormat),
+      trackList: files.filter((file) => file.format === selectedFormat),
     };
   }
 }
