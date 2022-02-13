94994424454a703ea013dae1041a835731911ea2
Jacob Greenwald <greenwald.j8@gmail.com>
2022-02-13 18:13:59 -0500

    comment out code

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,40 +1,41 @@
-import { Handler, Context } from 'aws-lambda';
-import { Server } from 'http';
-import { createServer, proxy } from 'aws-serverless-express';
-import { eventContext } from 'aws-serverless-express/middleware';
+// import { Handler, Context } from 'aws-lambda';
+// import { Server } from 'http';
+// import { createServer, proxy } from 'aws-serverless-express';
+// import { eventContext } from 'aws-serverless-express/middleware';
 
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
+// import { NestFactory } from '@nestjs/core';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import helmet from 'helmet';
 
-const express = require('express');
+// const express = require('express');
 
-const binaryMimeTypes: string[] = [];
+// const binaryMimeTypes: string[] = [];
 
-let cachedServer: Server;
+// let cachedServer: Server;
 
-async function bootstrapServer(): Promise<Server> {
-  if (!cachedServer) {
-    const expressApp = express();
-    const nestApp = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressApp),
-      { cors: true },
-    );
-    nestApp.useGlobalPipes(
-      new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-    );
-    nestApp.use(helmet());
-    nestApp.use(eventContext());
-    await nestApp.init();
-    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
-  }
-  return cachedServer;
-}
+// async function bootstrapServer(): Promise<Server> {
+//   if (!cachedServer) {
+//     const expressApp = express();
+//     const nestApp = await NestFactory.create(
+//       AppModule,
+//       new ExpressAdapter(expressApp),
+//       { cors: true },
+//     );
+//     nestApp.useGlobalPipes(
+//       new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+//     );
+//     nestApp.use(helmet());
+//     nestApp.use(eventContext());
+//     await nestApp.init();
+//     cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+//   }
+//   return cachedServer;
+// }
 
-export const handler: Handler = async (event: any, context: Context) => {
-  cachedServer = await bootstrapServer();
-  return proxy(cachedServer, event, context, 'PROMISE').promise;
-};
+// export const handler: Handler = async (event: any, context: Context) => {
+//   cachedServer = await bootstrapServer();
+//   return proxy(cachedServer, event, context, 'PROMISE').promise;
+// };
+export {};
