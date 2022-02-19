2f4e26b3a2f21da294b9dbba9e569a05730481d9
Jacob Greenwald <greenwald.j8@gmail.com>
2022-02-18 23:48:22 -0500

    Logging port

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3001;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(port);
 }
-bootstrap();
+bootstrap().then(() => console.log(`app listening on ${port}`));
