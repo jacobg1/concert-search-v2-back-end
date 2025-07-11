e3ebd4958a74982306a2b0c161cfad04f79f96b2
jacobg1 <greenwald.j8@gmail.com>
2025-07-10 20:46:48 -0400

    add mock entry point

---
+++ src/mocks/mockMain.ts
@@ -0,0 +1,21 @@
+import { handler as mainHandler } from '../main';
+import type {
+  APIGatewayProxyEventV2,
+  Callback,
+  Context,
+  Handler,
+} from 'aws-lambda';
+import { server } from './node';
+
+server.events.on('request:start', ({ request }) => {
+  console.log('MSW intercepted:', request.method, request.url);
+});
+
+export const handler: Handler = async (
+  event: APIGatewayProxyEventV2,
+  context: Context,
+  callback: Callback<any>,
+) => {
+  server.listen();
+  return mainHandler(event, context, callback);
+};
